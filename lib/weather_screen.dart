9211cb3019a8854204760782a3aa119ee11ee377
Michael Much. <michaelmuchiri857@gmail.com>
2026-07-31 15:54:50 +0300

    feat(UI): added a main card widget

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WeatherScreen extends StatelessWidget {
@@ -20,6 +22,53 @@ class WeatherScreen extends StatelessWidget {
             icon: const Icon(Icons.refresh),
           ),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // Main Card
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                elevation: 10,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            '300°F',
+                            style: TextStyle(
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 12),
+                          Icon(Icons.cloud, size: 64),
+                          SizedBox(height: 12),
+                          Text('Rain', style: TextStyle(fontSize: 20)),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Weather forecast
+            const Placeholder(fallbackHeight: 100),
+            const SizedBox(height: 20),
+            //Additional infomation
+            const Placeholder(fallbackHeight: 100),
+          ],
+        ),
       ),
     );
   }
