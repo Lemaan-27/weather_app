6bf82f4e6a95e75e7f68c196afa86fe6ef6b0f60
Michael Much. <michaelmuchiri857@gmail.com>
2026-08-03 14:37:31 +0300

    feat(UI): added weather forecast card

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -26,6 +26,7 @@ class WeatherScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // Main Card
             SizedBox(
@@ -40,7 +41,7 @@ class WeatherScreen extends StatelessWidget {
                   child: BackdropFilter(
                     filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
                     child: const Padding(
-                      padding: EdgeInsets.all(8.0),
+                      padding: EdgeInsets.all(6.0),
                       child: Column(
                         children: [
                           Text(
@@ -50,9 +51,9 @@ class WeatherScreen extends StatelessWidget {
                               fontWeight: FontWeight.bold,
                             ),
                           ),
-                          SizedBox(height: 12),
+                          SizedBox(height: 8),
                           Icon(Icons.cloud, size: 64),
-                          SizedBox(height: 12),
+                          SizedBox(height: 8),
                           Text('Rain', style: TextStyle(fontSize: 20)),
                         ],
                       ),
@@ -61,12 +62,57 @@ class WeatherScreen extends StatelessWidget {
                 ),
               ),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 16),
             // Weather forecast
-            const Placeholder(fallbackHeight: 100),
+            Text(
+              'Weather Forecast',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  HourlyForeCastItem(),
+                  HourlyForeCastItem(),
+                  HourlyForeCastItem(),
+                  HourlyForeCastItem(),
+                  HourlyForeCastItem(),
+                ],
+              ),
+            ),
+            // const Placeholder(fallbackHeight: 100),
             const SizedBox(height: 20),
             //Additional infomation
             const Placeholder(fallbackHeight: 100),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HourlyForeCastItem extends StatelessWidget {
+  const HourlyForeCastItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      child: Container(
+        width: 100,
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12)),
+        child: const Column(
+          children: [
+            Text(
+              '10:45',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Icon(Icons.cloud, size: 32),
+            SizedBox(height: 8),
+            Text('301.17'),
           ],
         ),
       ),
