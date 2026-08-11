c31bf2fcc1defd3b77365d9ff2b1484ea65db331
Michael Much. <michaelmuchiri857@gmail.com>
2026-08-11 10:30:58 +0300

    Added API

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,11 +1,38 @@
+import 'dart:convert';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:weather_app/addition_info_item.dart';
 import 'package:weather_app/hourly_weather_forecast.dart';
+import 'package:http/http.dart' as http;
+import 'package:weather_app/secrets.dart';
 
-class WeatherScreen extends StatelessWidget {
+class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
+
+  @override
+  State<WeatherScreen> createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  Future<Map<String, dynamic>> getCurrentWeather() async {
+    try {
+      String cityName = 'Kenya';
+      final res = await http.get(
+        Uri.parse(
+          'https://api.openweathermap.org/data/2.5/forecast?q=$cityName&APPID=$openWeatherAPIKey',
+        ),
+      );
+      final data = jsonDecode(res.body);
+      if (data["cod"] != "200") {
+        throw "An Unexpected Error Occoured!";
+      }
+      return data;
+      // data['list'][0]['main']['temp'];
+    } catch (e) {
+      throw e.toString();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,113 +52,136 @@ class WeatherScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Main Card
-            SizedBox(
-              width: double.infinity,
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                elevation: 10,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(16),
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                    child: const Padding(
-                      padding: EdgeInsets.all(6.0),
-                      child: Column(
-                        children: [
-                          Text(
-                            '300K',
-                            style: TextStyle(
-                              fontSize: 35,
-                              fontWeight: FontWeight.bold,
-                            ),
+      body: FutureBuilder(
+        future: getCurrentWeather(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator.adaptive());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text(snapshot.error.toString()));
+          }
+          final data = snapshot.data!;
+          final currentWeatherData = data['list'][0];
+          final currentTemp = currentWeatherData['main']['temp'];
+          final currentSky = currentWeatherData['weather'][0]['main'];
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Main Card
+                SizedBox(
+                  width: double.infinity,
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    elevation: 10,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                        child: Padding(
+                          padding: const EdgeInsets.all(6.0),
+                          child: Column(
+                            children: [
+                              Text(
+                                '$currentTemp K',
+                                style: const TextStyle(
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              const SizedBox(height: 8),
+                              Icon(
+                                currentSky == 'Clouds' || currentSky == 'Rain'
+                                    ? Icons.cloud
+                                    : Icons.sunny,
+                                size: 64,
+                              ),
+                              const SizedBox(height: 8),
+                              Text(
+                                currentSky,
+                                style: const TextStyle(fontSize: 20),
+                              ),
+                            ],
                           ),
-                          SizedBox(height: 8),
-                          Icon(Icons.cloud, size: 64),
-                          SizedBox(height: 8),
-                          Text('Rain', style: TextStyle(fontSize: 20)),
-                        ],
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ),
-            const SizedBox(height: 12),
-            // Weather forecast
-            const Text(
-              'Weather Forecast',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            const SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  HourlyForeCastItem(
-                    time: '09:00',
-                    icon: Icons.cloud,
-                    temp: '301.17',
-                  ),
-                  HourlyForeCastItem(
-                    time: '12:00',
-                    icon: Icons.sunny,
-                    temp: '301.54',
-                  ),
-                  HourlyForeCastItem(
-                    time: '15:00',
-                    icon: Icons.cloud,
-                    temp: '301.11',
-                  ),
-                  HourlyForeCastItem(
-                    time: '18:00',
-                    icon: Icons.sunny,
-                    temp: '300.79',
-                  ),
-                  HourlyForeCastItem(
-                    time: '21:00',
-                    icon: Icons.cloud,
-                    temp: '300.45',
-                  ),
-                ],
-              ),
-            ),
-            // const Placeholder(fallbackHeight: 100),
-            const SizedBox(height: 12),
-            const Text(
-              'Additional Infomation',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                AdditionalInfoItem(
-                  icon: Icons.water_drop,
-                  lable: 'Humidity',
-                  value: '94',
+                const SizedBox(height: 12),
+                // Weather forecast
+                const Text(
+                  'Weather Forecast',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
-                AdditionalInfoItem(
-                  icon: Icons.air,
-                  lable: 'Wind Speed',
-                  value: '7.67',
+                const SizedBox(height: 8),
+                const SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      HourlyForeCastItem(
+                        time: '09:00',
+                        icon: Icons.cloud,
+                        temp: '301.17',
+                      ),
+                      HourlyForeCastItem(
+                        time: '12:00',
+                        icon: Icons.sunny,
+                        temp: '301.54',
+                      ),
+                      HourlyForeCastItem(
+                        time: '15:00',
+                        icon: Icons.cloud,
+                        temp: '301.11',
+                      ),
+                      HourlyForeCastItem(
+                        time: '18:00',
+                        icon: Icons.sunny,
+                        temp: '300.79',
+                      ),
+                      HourlyForeCastItem(
+                        time: '21:00',
+                        icon: Icons.cloud,
+                        temp: '300.45',
+                      ),
+                    ],
+                  ),
                 ),
-                AdditionalInfoItem(
-                  icon: Icons.speed,
-                  lable: 'Pressure',
-                  value: '1006',
+                // const Placeholder(fallbackHeight: 100),
+                const SizedBox(height: 12),
+                const Text(
+                  'Additional Infomation',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 8),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    AdditionalInfoItem(
+                      icon: Icons.water_drop,
+                      lable: 'Humidity',
+                      value: '94',
+                    ),
+                    AdditionalInfoItem(
+                      icon: Icons.air,
+                      lable: 'Wind Speed',
+                      value: '7.67',
+                    ),
+                    AdditionalInfoItem(
+                      icon: Icons.speed,
+                      lable: 'Pressure',
+                      value: '1006',
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
